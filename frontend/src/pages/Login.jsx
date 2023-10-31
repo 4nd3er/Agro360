011fe266d1df5fb3c39a771a9c52ea5f27b17497
chantrecristian <118752898+chantrecristian@users.noreply.github.com>
2023-10-31 00:01:49 -0500

    subiendo cambios de la miin a la rama experimental

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,25 +4,23 @@ import { Link } from 'react-router-dom';
 const Login = () => {
     return (
         <>
-            <div className="flex  justify-center items-center min-h-[80vh]">
+            <div className="flex justify-center items-center min-h-[80vh]">
                 <div className="w-1/4 h-full flex flex-col justify-center">
-
-                    <h1 className="text-green-800 font-black text-6xl capitalize ">AGRO
+                    <h1 className="text-green-800 font-black text-6xl capitalize">
+                        AGRO
                         <span className="text-green-500 text-8xl">360°</span>
-                        <h3 className='text-green-800 text-sm flex justify-center '>Evaluacion de desempeño</h3>
                     </h1>
+                    <h3 className='text-green-800 text-sm flex justify-center '>Evaluación de desempeño</h3>
                 </div>
 
-
-                <div className="w-3/5 h-full ">
-                    <div className=" flex flex-col justify-center place-items-center min-h-[80vh] ">
-                        <strong className=' text-green-600  text-6xl capitalize font-sans'>Bienvenidos</strong>
-
-                        <form className='my-10 bg-white shadow rounded-lg px-10 py-5 w-1/2'/>
+                <div className="w-3/5 h-full">
+                    <div className="flex flex-col justify-center place-items-center min-h-[80vh]">
+                        <strong className='text-green-600 text-6xl capitalize font-sans'>Bienvenidos</strong>
+                        <form className='my-10 bg-white shadow rounded-lg px-10 py-5 w-1/2'>
                             <div className='my-5'>
-                                <label className='uppercase text-gray-600 block text-xl font-bold'
-                                    htmlFor='email'
-                                >Correo Electronico</label>
+                                <label className='uppercase text-gray-600 block text-xl font-bold' htmlFor='email'>
+                                    Correo Electrónico
+                                </label>
                                 <input
                                     id='email'
                                     type="text"
@@ -31,68 +29,36 @@ const Login = () => {
                                 />
                             </div>
                             <div className='my-5'>
-                                <label className='uppercase text-gray-600 block text-xl font-bold'
-                                    htmlFor='password'
-                                >Contraseña</label>
-
+                                <label className='uppercase text-gray-600 block text-xl font-bold' htmlFor='password'>
+                                    Contraseña
+                                </label>
                                 <input
                                     id='password'
                                     type="password"
                                     placeholder='Password de Registro'
                                     className='w-full mt-3 p-3 border rounded-xl bg-gray-50'
                                 />
-                                <Link
-                                    className='block  my-5 text-slate-500 uppercase text-xs'
-
-                                    to='olvide-password '
-                                >¿Olvidaste tu contraseña?</Link>
-                            </div>
-                            <Link to='/inicio' >
-                                <input 
-                                type="button" 
-                                value="Iniciar Sesión"
-                                className='bg-green-600 w-full py-1 text-white uppercase font-bold rounded-xl
-                                                hover: cursor-pointer hover:bg-green-700 transition-color'
-                                />
-                            </Link >
-
-                            <Link
-                                className='block my-5 text-slate-500 uppercase text-xs'
-                                to='registrar'
-
-                                    to='forget-password'
-                                >¿Olvidaste tu contraseña?</Link>
+                                <Link className='block my-5 text-slate-500 uppercase text-xs' to='/olvide-password'>
+                                    ¿Olvidaste tu contraseña?
+                                </Link>
                             </div>
                             <input
                                 type="submit"
-                                value="Iniciar Sesion"
+                                value="Iniciar Sesión"
                                 className='bg-green-600 w-full py-1 text-white uppercase font-bold rounded-xl
-                hover: cursor-pointer hover:bg-green-700 transition-color'
+                                cursor-pointer hover:bg-green-700 transition-color'
                             />
-                            <Link
-                                className='block my-5 text-slate-500 uppercase text-xs'
-                                to='register'
-
-                            >¿No tiene una cuenta? Registrarse</Link>
+                            <Link className='block my-5 text-slate-500 uppercase text-xs' to='/registrar'>
+                                ¿No tienes una cuenta? Regístrate
+                            </Link>
                         </form>
-
-                        <nav className='lg:flex lg: justify-between'>
-
-
+                        <nav className='lg:flex lg:justify-between'>
                         </nav>
-
                     </div>
-
                 </div>
             </div>
-
-
-
         </>
-
-
-
-    )
+    );
 }
 
-export default Login
+export default Login;
