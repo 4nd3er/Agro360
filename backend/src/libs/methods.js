1f9c44f9d5b6674552497c20078f1340bc3cf992
Giovanny Ladino <ladigiococ@gmail.com>
2023-11-07 12:10:55 -0500

    Cambios en la funcion methods

--- backend/src/libs/methods.js
+++ backend/src/libs/methods.js
@@ -16,6 +16,7 @@ export const getMethod = async (res, model, name) => {
     }
 }
 
+
 export const getOneMethod = async (id, res, model, name) => {
 
     const mayusName = name.charAt(0).toUpperCase() + name.slice(1)
@@ -82,7 +83,7 @@ export const updateMethod = async (data, id, find, res, model, name) => {
         if (!validObjectId(id)) return res.status(400).json({ msg: messages.invalidId(name) })
 
         const findExistsModel = await model.findOne(find)
-        if (findExistsModel) return res.status(400).json({ msg: messages.alreadyExists(mayusName) })
+        if (findExistsModel && findExistsModel._id.toString() !== id) return res.status(400).json({ msg: messages.alreadyExists(mayusName) })
 
         const findModel = await model.findByIdAndUpdate(id, data, {
             new: true
@@ -91,6 +92,34 @@ export const updateMethod = async (data, id, find, res, model, name) => {
 
         res.json({
             response: `${mayusName} updated successfully`,
+            data: findModel
+        })
+    } catch (error) {
+        errorResponse(res, error)
+    }
+}
+
+export const updateMethodList = async (res, reference, data, find, model, names) => {
+    
+    const id = Object.values(reference)[0]
+    const idList = Object.values(reference)[1]
+
+    const name = Object.values(names)[0]
+    const nameList = Object.values(names)[1]
+    try {
+        if (!validObjectId(id)) return res.status(400).json({ msg: messages.invalidId(name) })
+        if (!validObjectId(idList)) return res.status(400).json({ msg: messages.invalidId(nameList) })
+    
+        const findExistsList = await model.findOne(find)
+        if (findExistsList && findExistsList._id.toString() !== idList) return res.status(400).json({ msg: messages.alreadyExists(nameList) })
+        
+        const findModel = await model.findOneAndUpdate(reference, data, {
+            new: true
+        })
+        if (!findModel) return res.status(404).json({ msg: messages.notFound(name) })
+
+        res.json({
+            response: `${nameList} updated successfully`,
             data: findModel
         })
     } catch (error) {
