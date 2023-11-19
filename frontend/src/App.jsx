7531f05ffa1cebb32a54adf57e28842bde2a63a7
Giovanny Ladino <ladigiococ@gmail.com>
2023-11-19 14:11:33 -0500

    Correcion error de importacion Autprovider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AuthLayout from './layouts/AuthLayout';
-import {AuthProvider} from './context/RolesProvider';
+import { AuthProvider } from './context/AuthContex';
 import LayoutLogin from './layouts/LayoutLogin';
 import { RolesProvider } from './context/RolesProvider';
 import {
@@ -21,32 +21,32 @@ console.log(import.meta.env.VITE_BACKEND_URL)
 
 function App() {
   return (
-   <AuthProvider>
-     <BrowserRouter>
-      <RolesProvider>
-        <Routes>
-          <Route path='/' element={<LayoutLogin />}>
-            <Route index element={<Login />} />
-            <Route path='register' element={<Register />} />
-            <Route path='forget-password' element={<ForgetPassword />} />
-            <Route path='forget-password/:token' element={<NewPassword />} />
-            <Route path='confirm/:id' element={<ConfirmAccount />} />
-          </Route>
-          <Route path='/inicio' element={<AuthLayout />}>
-            <Route index element={<Home />} />
-            <Route path=':id/tematicas' element={<Topics />}/>
-          </Route>
-          <Route path='/crear-formulario' element={<AuthLayout />}>
-            <Route index element={<Quest />} />
-            <Route path='crear' element={<CreateQuest />} />
-          </Route>
-          <Route path='/resultados' element={<AuthLayout />}>
-            <Route index element={<Results />} />
-          </Route>
-        </Routes>
-      </RolesProvider>
-    </BrowserRouter>
-   </AuthProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <RolesProvider>
+          <Routes>
+            <Route path='/' element={<LayoutLogin />}>
+              <Route index element={<Login />} />
+              <Route path='register' element={<Register />} />
+              <Route path='forget-password' element={<ForgetPassword />} />
+              <Route path='forget-password/:token' element={<NewPassword />} />
+              <Route path='confirm/:id' element={<ConfirmAccount />} />
+            </Route>
+            <Route path='/inicio' element={<AuthLayout />}>
+              <Route index element={<Home />} />
+              <Route path=':id/tematicas' element={<Topics />} />
+            </Route>
+            <Route path='/crear-formulario' element={<AuthLayout />}>
+              <Route index element={<Quest />} />
+              <Route path='crear' element={<CreateQuest />} />
+            </Route>
+            <Route path='/resultados' element={<AuthLayout />}>
+              <Route index element={<Results />} />
+            </Route>
+          </Routes>
+        </RolesProvider>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
