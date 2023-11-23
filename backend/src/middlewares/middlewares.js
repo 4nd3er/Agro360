3d949bade3fa3b514986eb2bb1a522262d92ee89
Giovanny Ladino <ladigiococ@gmail.com>
2023-11-22 20:52:28 -0500

    Solucion temporal: error al leer tokeen

--- backend/src/middlewares/middlewares.js
+++ backend/src/middlewares/middlewares.js
@@ -3,13 +3,14 @@ import { SECRET_TOKEN } from '../config/config.js'
 
 //* Funcion para validar un token de una cookie
 export const validateTokenCookie = (req, res, next) => {
-    const { token } = req.cookies
+    /*const { token } = req.cookies
     if (!token) return res.status(401).json({ message: ["No token, authorization denied"] });
     jwt.verify(token, SECRET_TOKEN, (err, user) => {
         if (err) return res.status(403).json({ message: ["Invalid token"] });
         req.user = user;
         next();
-    })
+    })*/
+    next();
 }
 
 //* Funcion para validar un token por parametro
