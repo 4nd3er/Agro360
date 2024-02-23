112cea187eb4a21e6fd3d88fa2b4656440f666f4
chantrecristian <118752898+chantrecristian@users.noreply.github.com>
2024-02-23 13:25:17 -0500

    implementacion responsive panel-administrador a el archivo ForgetPassword.jsx

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["index.html", "./src/**/*.jsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'work-sans': ['Work Sans', 'sans'],
+        'josefin-sans': ['Josefin Sans', 'sans']
+      },
+      boxShadow: {
+        'shadow-button': '0px 5px 5px rgba(0, 0, 0, 0.5)',
+      },
+      colors: {
+        'color-sena': '#39a900',
+        'color-sena-hover': '#369206',
+        'color-aprendiz': '#82DEF0',
+        'color-instructor': '#CB7766',
+        'color-directivo': '#385C57',
+      },
+    },
+  },
+  plugins: [],
+}
+
